dac8daca62aa5ee28a01a3e2ae607de58fdb4389
Dmitriy <dimk00z@gmail.com>
2023-08-10 09:42:36 +0300

    2_1 update

--- src/2_1/readrides.py
+++ src/2_1/readrides.py
@@ -1,10 +1,41 @@
 import csv
-from pathlib import Path
-from typing import NamedTuple, Type
+import time
 from dataclasses import dataclass
+from functools import wraps
+from pathlib import Path
+from typing import Any, NamedTuple, Type
+from msgspec import Struct
+from pydantic import BaseModel
+
+
+def timeit(func):
+    @wraps(func)
+    def timeit_wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        print(f"Function {func.__name__}{args} {kwargs} Took {total_time:.4f} seconds")
+        return result
+
+    return timeit_wrapper
 
 
 class RowTuple(NamedTuple):
+    route: str
+    date: str
+    daytype: str
+    rides: int
+
+
+class RowPydantic(BaseModel):
+    route: str
+    date: str
+    daytype: str
+    rides: int
+
+
+class RowStruct(Struct):
     route: str
     date: str
     daytype: str
@@ -49,18 +80,26 @@ class RowWithSlots:
 
 data_path = Path("./Data/ctabus.csv")
 
+TYPES_CLASSES = (
+    RowTuple,
+    RowSimple,
+    RowWithSlots,
+    RowDataclassWithotSlots,
+    RowDataclassWithSlots,
+    RowPydantic,
+    RowStruct,
+)
+TYPES = (
+    tuple,
+    dict,
+    *TYPES_CLASSES,
+)
 
+
+@timeit
 def read_rides_as_tuples(
     filename,
-    result_type: Type[
-        tuple
-        | dict
-        | RowTuple
-        | RowSimple
-        | RowWithSlots
-        | RowDataclassWithSlots
-        | RowDataclassWithotSlots
-    ],
+    result_type: Type[Any],
 ):
     """
     Read the bus ride data as a list of tuples
@@ -75,15 +114,14 @@ def read_rides_as_tuples(
             date = row[1]
             daytype = row[2]
             rides = int(row[3])
-            if result_type in (
-                RowTuple,
-                RowSimple,
-                RowWithSlots,
-                RowDataclassWithotSlots,
-                RowDataclassWithSlots,
-            ):
+            if result_type in TYPES_CLASSES:
                 #     case tuple:
-                record = result_type(route, date, daytype, rides)
+                record = result_type(
+                    route=route,
+                    date=date,
+                    daytype=daytype,
+                    rides=rides,
+                )
                 records.append(record)
 
             if result_type == tuple:
@@ -97,38 +135,37 @@ def read_rides_as_tuples(
                         "rides": rides,
                     }
                 )
+    print(f"Total {len(records)}")
     return records
 
 
 if __name__ == "__main__":
     import tracemalloc
 
-    for res_type in (
-        tuple,
-        dict,
-        RowTuple,
-        RowSimple,
-        RowWithSlots,
-        RowDataclassWithotSlots,
-        RowDataclassWithSlots,
-    ):
+    for res_type in TYPES:
         tracemalloc.start()
         rows = read_rides_as_tuples("Data/ctabus.csv", result_type=res_type)
-        print("Memory Use: Current %d, Peak %d" % tracemalloc.get_traced_memory())
+        current, peak = tracemalloc.get_traced_memory()
+        print(f"Memory Use: Current {current}, Peak {peak}")
         tracemalloc.stop()
 
-
 # Result type <class 'tuple'>
-# Memory Use: Current 123688286, Peak 123718599
+# Memory Use: Current 123688139, Peak 123718519
 # Result type <class 'dict'>
-# Memory Use: Current 188375302, Peak 188405671
+# Memory Use: Current 188375438, Peak 188405807
 # Result type <class '__main__.RowTuple'>
-# Memory Use: Current 128309014, Peak 128339311
+# Memory Use: Current 128309110, Peak 128339407
 # Result type <class '__main__.RowSimple'>
-# Memory Use: Current 142173206, Peak 142203495
+# Memory Use: Current 142173254, Peak 142203543
 # Result type <class '__main__.RowWithSlots'>
-# Memory Use: Current 119068110, Peak 119098391
+# Memory Use: Current 119068038, Peak 119098319
 # Result type <class '__main__.RowDataclassWithotSlots'>
-# Memory Use: Current 142173334, Peak 142203607
+# Memory Use: Current 142173318, Peak 142203591
 # Result type <class '__main__.RowDataclassWithSlots'>
-# Memory Use: Current 119068110, Peak 119098375
+# Memory Use: Current 119067982, Peak 119098247
+# Result type <class '__main__.RowPydantic'> -v2
+# Memory Use: Current 359334494, Peak 359364807
+# Result type <class '__main__.RowPydantic'> -v1
+# Memory Use: Current 345473582, Peak 345503895
+# Result type <class '__main__.RowStruct'>
+# Memory Use: Current 119067750, Peak 119098055
