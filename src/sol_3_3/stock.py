0085de7a2c41928ecc54d58558d0da1da11dd2ef
Dmitriy <dimk00z@gmail.com>
2023-08-16 15:00:42 +0300

    Add 3_3

---
+++ src/sol_3_3/stock.py
@@ -0,0 +1,44 @@
+class Stock:
+    types = (str, int, float)
+
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+
+    def cost(self):
+        return self.shares * self.price
+
+    def sell(self, nshares):
+        self.shares -= nshares
+
+    @classmethod
+    def from_row(cls, row):
+        values = [func(val) for func, val in zip(cls.types, row)]
+        return cls(*values)
+
+
+def read_portfolio(filename):
+    """
+    Read a CSV file of stock data into a list of Stocks
+    """
+    import csv
+
+    portfolio = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        next(rows)
+        for row in rows:
+            record = Stock.from_row(row)
+            portfolio.append(record)
+    return portfolio
+
+
+if __name__ == "__main__":
+    from src.sol_3_2 import tableformat
+
+    portfolio = read_portfolio("Data/portfolio.csv")
+
+    # Generalized version
+    # portfolio = reader.read_csv_as_instances('Data/portfolio.csv', Stock)
+    tableformat.print_table(portfolio, ["name", "shares", "price"])
