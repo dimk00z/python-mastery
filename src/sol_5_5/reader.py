11740347cd928c2c911fd5849309b8df5ea0ce34
Dmitriy <dimk00z@gmail.com>
2023-08-27 10:21:38 +0300

    Add 5_5

--- src/sol_5_5/reader.py
+++ src/sol_5_5/reader.py
@@ -1,7 +1,11 @@
 import csv
+import logging
 from typing import Any, Callable, Iterable
 
 import stock
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 
 def convert_csv(
@@ -9,19 +13,26 @@ def convert_csv(
     converter: Callable[[list[str], list[str]], Any],
     *,
     headers=None
-):
+) -> list[Any]:
     """
     Convert lines of CSV data into data that depends on converter.
     """
     rows = csv.reader(lines)
     if headers is None:
         headers = next(rows)
-    return list(map(lambda row: converter(headers, row), rows))
+    results = []
+    for index, row in enumerate(rows, start=1):
+        try:
+            results.append(converter(headers, row))
+        except ValueError as ex:
+            logger.info("Row %d: Bad row: %s", index, row)
+            logger.debug("Row %d Reason: %s", index, ex)
+    return results
 
 
 def csv_as_dicts(
     lines: Iterable[str], types: Iterable[type], headers: Iterable[str] | None = None
-) -> list[dict[str, Any]]:
+) -> list[dict[str, Any] | Any]:
     """
     Convert lines of CSV data into a list of dictionaries
     """
