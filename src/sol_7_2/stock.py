8cd8f816226fc38235c5fc2985262773e5b3aabe
Dmitriy <dimk00z@gmail.com>
2023-09-06 10:56:18 +0300

    Add 7_2

---
+++ src/sol_7_2/stock.py
@@ -0,0 +1,19 @@
+from structure import Structure
+from validate import Integer, validated
+
+
+class Stock(Structure):
+    _fields = ("name", "shares", "price")
+
+    @property
+    def cost(self):
+        return self.shares * self.price
+
+    @validated
+    def sell(self, nshares: Integer):
+        self.shares -= nshares
+
+    # sell = ValidatedFunction(sell)
+
+
+Stock.create_init()
