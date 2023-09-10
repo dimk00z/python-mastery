e6cad9295b10c3b418009c7746156bdf2c95302a
Dmitriy <dimk00z@gmail.com>
2023-09-10 17:01:39 +0300

    Update 8_1 8_2

---
+++ src/sol_8_2/stock.py
@@ -0,0 +1,15 @@
+from structure import Structure
+from validate import PositiveFloat, PositiveInteger, String
+
+
+class Stock(Structure):
+    name = String()
+    shares = PositiveInteger()
+    price = PositiveFloat()
+
+    @property
+    def cost(self):
+        return self.shares * self.price
+
+    def sell(self, nshares: PositiveInteger):
+        self.shares -= nshares
